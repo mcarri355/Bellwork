5152e7410fcd8563f4dbcba4f96ce0a422f5af09
mcarri355 <carrillomatisa@gmail.com>
2023-03-09 17:47:29 -0700

    3-8 bellwork

---
+++ 3-8/3-8.js
@@ -0,0 +1,50 @@
+
+
+function area(){
+    let triangle = (1/2 * 12 * 18);
+    document.getElementById("area").value = triangle;
+    console.log(triangle)
+}
+
+function date(){
+    let d = new Date();
+    let time = d.toTimeString();
+    let day;
+    if(d.getDay() == 0){
+        day = "Sunday";
+    }
+    else if(d.getDay() == 1){
+        day = "Monday";
+    }
+    else if(d.getDay() == 2){
+        day = "Tuesday";
+    }
+    else if(d.getDay() == 3){
+        day = "Wednesday";
+    }
+    else if(d.getDay() == 4){
+        day = "Thurdsay";
+    }
+    else if(d.getDay() == 5){
+        day = "Friday";
+    }
+    else if(d.getDay() == 5){
+        day = "Saturday";
+    }
+    else {
+        day = "Sunday"
+    }
+    document.getElementById("date").value = "Today's Date: " + day + ". Current Time: " + time;
+}
+
+function c2f(){
+    let cel = document.getElementById("cel").value
+    document.getElementById("c2f").value = 
+    ((cel * 9/5) + 32)  + " Degrees Farenheit "
+}
+
+function f2c(){
+    let far = document.getElementById("far").value
+    document.getElementById("f2c").value = ((far - 32) * 5/9)
+ + " Degrees Celsius "
+}
