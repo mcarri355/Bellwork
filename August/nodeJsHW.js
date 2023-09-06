d7b961e2cd10a588f93f4badf94e775d368321a3
mcarri355 <carrillomatisa@gmail.com>
2023-09-06 15:23:34 -0700

    bell

--- August/nodeJsHW.js
+++ August/nodeJsHW.js
@@ -2,69 +2,69 @@ const fs = require('fs');
 const path = require('path');
 
 fs.renameSync(
-  path.join(__dirname, 'New-Text-Document.txt'),
-  path.join(__dirname, 'Latin-Gobbly-Gook.txt'),
+  path.join(__dirname, './August/New-Text-Document.txt'),
+  path.join(__dirname, './August/Latin-Gobbly-Gook.txt'),
   function (err) {
     if (err) {
       console.log(err);
-      return;
     }
   }
 );
 
 fs.renameSync(
-  path.join(__dirname, 'New-Text-Document2.txt'),
-  path.join(__dirname, 'Princess-Bubblegum-Is-A-Problem.txt'),
+  path.join(__dirname, './August/New-Text-Document2.txt'),
+  path.join(__dirname, './August/Princess-Bubblegum-Is-A-Problem.txt'),
   function (err) {
     if (err) {
       console.log(err);
-      return;
     }
   }
 );
 
 fs.renameSync(
-  path.join(__dirname, 'New-Rich-Text.rtf'),
-  path.join(__dirname, 'Learning-Is-Hard.txt'),
+  path.join(__dirname, './August/New-Rich-Text.rtf'),
+  path.join(__dirname, './August/Learning-Is-Hard.txt'),
   function (err) {
     if (err) {
       console.log(err);
-      return;
     }
   }
 );
 
 fs.renameSync(
-  path.join(__dirname, '/tryThis/New-Document.txt'),
-  path.join(__dirname, '/tryThis/Regular-Bros-Show.txt'),
+  path.join(__dirname, './August/tryThis/New-Document.txt'),
+  path.join(__dirname, './August/tryThis/Regular-Bros-Show.txt'),
   function (err) {
     if (err) {
       console.log(err);
-      return;
     }
   }
 );
 
 fs.rename(
-  path.join(dirname, '/tryThis/Regular Bros Show.txt'),
-  path.join(dirname, '/Regular Bros Show.txt')
+  path.join(__dirname, './August/tryThis/Regular Bros Show.txt'),
+  path.join(__dirname, './August/Regular Bros Show.txt')
 );
-fs.rmdir(path.join(dirname, '/tryThis'), function () {
+
+fs.rmdir(path.join(__dirname, '/tryThis'), function () {
   console.log('Success');
 });
 fs.rename(
-  path.join(dirname, '/New Folder'),
-  path.join(dirname, '/Answer')
+  path.join(__dirname, './August/New Folder'),
+  path.join(__dirname, './August/Answer')
 );
-let e = fs.readdirSync(dirname);
+let e = fs.readdirSync(__dirname);
 let u = [];
 for (let y = e.length - 1; y > e.length - 5; y--) {
   u.unshift(
-    fs.readFileSync(path.join(dirname, '/' + e[y]), 'utf-8').split(' ')[
-      fs.readFileSync(path.join(dirname, '/' + e[y]), 'utf-8').split(' ')
+    fs.readFileSync(path.join(__dirname, '/' + e[y]), 'utf-8').split(' ')[
+      fs.readFileSync(path.join(__dirname, '/' + e[y]), 'utf-8').split(' ')
         .length - 1
     ]
   );
 }
 console.log(u);
-fs.writeFileSync(path.join(__dirname, '/Answer/answer.js'), u.join(' '));
+fs.writeFileSync(
+  path.join(__dirname, './August/Answer/answer.js'),
+  u.join(' ')
+);
